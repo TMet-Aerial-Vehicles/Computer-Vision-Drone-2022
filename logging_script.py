7bb6ac53741a8f5a06c51a6bf3938acbc7d92da3
Craig <craigpinto17@gmail.com>
2022-04-14 15:10:55 -0400

    Changed logger handler

--- logging_script.py
+++ logging_script.py
@@ -5,6 +5,12 @@ from datetime import datetime
 # noinspection PyArgumentList
 def start_logging():
     file = f"logging/logging-{datetime.today().strftime('%Y-%m-%d-%H')}.log"
-    logging.basicConfig(encoding='utf-8', filename=file, level=logging.DEBUG,
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # logging.basicConfig(encoding='utf-8', filename=file, level=logging.DEBUG,
+    #                     format='%(asctime)s - %(levelname)s - %(message)s',
+    #                     datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    root_logger= logging.getLogger()
+    root_logger.setLevel(logging.DEBUG) # or whatever
+    handler = logging.FileHandler(file, 'w', 'utf-8') # or whatever
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    root_logger.addHandler(handler)
