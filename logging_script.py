08e8a93b94bf3a63eb6ca9b7e077814de9e2d580
Craig <craigpinto17@gmail.com>
2022-04-14 15:42:45 -0400

    Fixing import error

--- logging_script.py
+++ logging_script.py
@@ -1,10 +1,16 @@
 import logging
 from datetime import datetime
 
+raspberry_pi = False        # True if running on raspberry pi
+
 
 # noinspection PyArgumentList
-def start_logging(dir='logging/'):
-    file = f"{dir}logging-{datetime.today().strftime('%Y-%m-%d-%H')}.log"
+def start_logging():
+    if raspberry_pi:
+        directory = '/home/pi/Desktop/JTC_ComputerVision/'
+    else:
+        directory = ''
+    file = f"{directory}logging-{datetime.today().strftime('%Y-%m-%d-%H')}.log"
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(file, 'w', 'utf-8')
